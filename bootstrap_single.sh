575ae1d5ce5ac39353680ae2a98567dac8c082a2
Lovetheminers <128461027+Lovetheminers@users.noreply.github.com>
2023-03-21 08:58:28 +0100

    Update bootstrap_single.sh

--- bootstrap_single.sh
+++ bootstrap_single.sh
@@ -2,14 +2,14 @@
 
 ################################################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox #
-# Updated by Dirty Harry for YiiMP use...                                      #
+# Updated by Lovetheminers for YiiMP use...                                      #
 # This script is intended to be ran from the Yiimp Server Installer            #
 ################################################################################
 
 # Clone the Yiimp repository if it doesn't exist.
 if [ ! -d $HOME/yiimpserver/yiimp_single ]; then
 	echo Downloading Dirty Harry Yiimp Single Server Installer v1.0. . .
-	git clone https://github.com/DirtyHarryDev/server_yiimp_single "$HOME"/yiimpserver/yiimp_single < /dev/null 2> /dev/null
+	git clone https://github.com/Lovetheminers/server_yiimp_single "$HOME"/yiimpserver/yiimp_single < /dev/null 2> /dev/null
 	echo
 fi
 
